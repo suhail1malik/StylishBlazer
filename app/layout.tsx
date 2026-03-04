081719205527f9839b69867eecae2a6c5ba4923b
suhail1malik <suhailmalik7643@gmail.com>
2026-03-05 00:38:40 +0530

    fix: remove horizontal scrollbar on mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} overflow-x-hidden bg-slate-50`}>{children}</body>
     </html>
   );
 }
